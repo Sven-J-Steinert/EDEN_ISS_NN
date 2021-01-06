a2b51608f020b593aad562a75a919640c0c1d19c
Sven Steinert <svensteinert96@gmail.com>
2021-01-06 12:07:03 +0100

    TCS read in

--- build_dataset.py
+++ build_dataset.py
@@ -30,7 +30,7 @@ dataset_GMTF = raw_data_GMTF.copy()
 
 print('EDIT', end=' ', flush=True)
 datetime_GMTF = pd.to_datetime(dataset_GMTF.index)
-datetime_GMTF = pd.DatetimeIndex(((datetime_GMTF.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
+#datetime_GMTF = pd.DatetimeIndex(((datetime_GMTF.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
 datetime_GMTF = pd.DatetimeIndex(((datetime_GMTF.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
 dataset_GMTF.index = datetime_GMTF
 
@@ -75,7 +75,7 @@ dataset_AMS = raw_data_AMS.copy()
 
 print('EDIT', end=' ', flush=True)
 datetime_AMS = pd.to_datetime(dataset_AMS.index)
-datetime_AMS = pd.DatetimeIndex(((datetime_AMS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
+#datetime_AMS = pd.DatetimeIndex(((datetime_AMS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
 datetime_AMS = pd.DatetimeIndex(((datetime_AMS.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
 dataset_AMS.index = datetime_AMS
 
@@ -105,6 +105,55 @@ print(int(dataset_AMS.isnull().sum().sum()/dataset_AMS.shape[1]), end=' | ')
 print("%.2f" % ((int(dataset_AMS.isnull().sum().sum()/dataset_AMS.shape[1])*100) / dataset_AMS.shape[0]), end=' ')
 print('%')
 
+
+
+print('')
+print('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
+print("LOADING ILS", end=' ', flush=True)
+url_ILS = 'data_ILS.csv'
+
+raw_data_ILS = pd.read_csv(url_ILS,dayfirst=True,
+    parse_dates=[['Date', 'Time']], index_col="Date_Time", na_values='?',
+    comment='\t', sep=';', skipinitialspace=True, low_memory=False)
+
+print(raw_data_ILS.shape, end=' ', flush=True)
+dataset_ILS = raw_data_ILS.copy()
+
+print('EDIT', end=' ', flush=True)
+datetime_ILS = pd.to_datetime(dataset_ILS.index)
+#datetime_ILS = pd.DatetimeIndex(((datetime_ILS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
+datetime_ILS = pd.DatetimeIndex(((datetime_ILS.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
+dataset_ILS.index = datetime_ILS
+
+dataset_ILS = dataset_ILS[~dataset_ILS.index.duplicated(keep='first')]  # remove duplicate indexes
+
+
+del dataset_ILS['SampleNumber']
+print(dataset_ILS.shape, end=' ', flush=True)
+
+dataset_ILS = pd.concat([timeframe, dataset_ILS], axis=1, sort=False, join='outer')
+dataset_ILS.index = pd.to_datetime(dataset_ILS.index)
+dataset_ILS.index.name = 'Date_Time'
+del dataset_ILS['Placeholder']
+
+print('APPLYED ON timeframe', end=' ', flush=True)
+print(dataset_ILS.shape)
+
+
+
+print('')
+print(dataset_ILS.head())
+print(dataset_ILS.tail())
+print('')
+print('missing')
+print('number', end=' ')
+print(dataset_ILS.isnull().sum().sum(), end=' ')
+print('| lines', end=' ')
+print(int(dataset_ILS.isnull().sum().sum()/dataset_ILS.shape[1]), end=' | ')
+print("%.2f" % ((int(dataset_ILS.isnull().sum().sum()/dataset_ILS.shape[1])*100) / dataset_ILS.shape[0]), end=' ')
+print('%')
+
+
 print('')
 print('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
 print("LOADING NDS", end=' ', flush=True)
@@ -120,7 +169,7 @@ dataset_NDS = raw_data_NDS.copy()
 
 print('EDIT', end=' ', flush=True)
 datetime_NDS = pd.to_datetime(dataset_NDS.index)
-datetime_NDS = pd.DatetimeIndex(((datetime_NDS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
+#datetime_NDS = pd.DatetimeIndex(((datetime_NDS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
 datetime_NDS = pd.DatetimeIndex(((datetime_NDS.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
 dataset_NDS.index = datetime_NDS
 
@@ -153,49 +202,50 @@ print('%')
 
 print('')
 print('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
-print("LOADING ILS", end=' ', flush=True)
-url_ILS = 'data_ILS.csv'
+print("LOADING TCS", end=' ', flush=True)
+url_TCS = 'data_TCS.csv'
 
-raw_data_ILS = pd.read_csv(url_ILS,dayfirst=True,
+raw_data_TCS = pd.read_csv(url_TCS, dayfirst=True,
     parse_dates=[['Date', 'Time']], index_col="Date_Time", na_values='?',
     comment='\t', sep=';', skipinitialspace=True, low_memory=False)
 
-print(raw_data_ILS.shape, end=' ', flush=True)
-dataset_ILS = raw_data_ILS.copy()
+print(raw_data_TCS.shape, end=' ', flush=True)
+dataset_TCS = raw_data_TCS.copy()
+
 
 print('EDIT', end=' ', flush=True)
-datetime_ILS = pd.to_datetime(dataset_ILS.index)
-datetime_ILS = pd.DatetimeIndex(((datetime_ILS.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
-datetime_ILS = pd.DatetimeIndex(((datetime_ILS.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
-dataset_ILS.index = datetime_ILS
+datetime_TCS = pd.to_datetime(dataset_TCS.index)
+#datetime_GMTF = pd.DatetimeIndex(((datetime_GMTF.asi8/(1e9*60)).round()*1e9*60).astype(np.int64))  # round to flat minutes
+datetime_TCS = pd.DatetimeIndex(((datetime_TCS.asi8/(1e10*30)).round()*1e10*30).astype(np.int64))  # round to 5 minute steps
+dataset_TCS.index = datetime_TCS
 
-dataset_ILS = dataset_ILS[~dataset_ILS.index.duplicated(keep='first')]  # remove duplicate indexes
+dataset_TCS = dataset_TCS[~dataset_TCS.index.duplicated(keep='first')]  # remove duplicate indexes
 
+del dataset_TCS['SampleNumber'] # delete SampleNumber
 
-del dataset_ILS['SampleNumber']
-print(dataset_ILS.shape, end=' ', flush=True)
+print(dataset_TCS.shape, end=' ', flush=True)
 
-dataset_ILS = pd.concat([timeframe, dataset_ILS], axis=1, sort=False, join='outer')
-dataset_ILS.index = pd.to_datetime(dataset_ILS.index)
-dataset_ILS.index.name = 'Date_Time'
-del dataset_ILS['Placeholder']
+dataset_TCS = pd.concat([timeframe, dataset_TCS], axis=1, sort=False, join='outer')
+dataset_TCS.index = pd.to_datetime(dataset_TCS.index)
+dataset_TCS.index.name = 'Date_Time'
+del dataset_TCS['Placeholder']
 
 print('APPLYED ON timeframe', end=' ', flush=True)
-print(dataset_ILS.shape)
-
-
+print(dataset_TCS.shape)
 
 print('')
-print(dataset_ILS.head())
-print(dataset_ILS.tail())
+print(dataset_TCS.head())
+print(dataset_TCS.tail())
 print('')
 print('missing')
 print('number', end=' ')
-print(dataset_ILS.isnull().sum().sum(), end=' ')
+print(dataset_TCS.isnull().sum().sum(), end=' ')
 print('| lines', end=' ')
-print(int(dataset_ILS.isnull().sum().sum()/dataset_ILS.shape[1]), end=' | ')
-print("%.2f" % ((int(dataset_ILS.isnull().sum().sum()/dataset_ILS.shape[1])*100) / dataset_ILS.shape[0]), end=' ')
-print('%')
+print(int(dataset_TCS.isnull().sum().sum()/dataset_TCS.shape[1]), end=' | ')
+print("%.2f" % ((int(dataset_TCS.isnull().sum().sum()/dataset_TCS.shape[1])*100) / dataset_TCS.shape[0]), end=' ')
+print('%', end='  ---> DROPPED!')
+print('')
+
 
 ###############################################################################
 # COMBINE DATA
@@ -203,17 +253,19 @@ print('%')
 print('')
 print('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────')
 print('COMBINING to final dataset', end=' ', flush = True)
-dataset = pd.concat([timeframe, dataset_GMTF, dataset_AMS, dataset_NDS, dataset_ILS], axis=1, sort=False, join='outer')
+dataset = pd.concat([timeframe, dataset_GMTF, dataset_AMS, dataset_ILS, dataset_NDS], axis=1, sort=False, join='outer')
 dataset.index = pd.to_datetime(dataset.index)
 dataset.index.name = 'Date_Time'
 del dataset['Placeholder']
 print(dataset.shape, end=' ', flush = True)
-print(' | order: GTMF AMS NDS ILS')
+print(' | order: GTMF AMS ILS NDS   (TCS dropped)')
 print(dataset.head(10))
 print(dataset.tail(10))
 print('')
 print('missing numbers:', end=' ')
-print(dataset.isnull().sum().sum())
+print(dataset.isnull().sum().sum(), end=' | ')
+print("%.2f" % ((dataset.isnull().sum().sum()/(dataset.shape[1]*dataset.shape[0]))*100), end=' ')
+print('%')
 
 ###############################################################################
 # WRITE DATASET
